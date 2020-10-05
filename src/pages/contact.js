278312830c7f87e4aca56ca38ccd400ffb173704
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-10-05 14:55:09 +0200

    Create Contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from "gatsby"
+import { Formik, useField } from 'formik';
+import * as Yup from "yup";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+const ContactPage = () => {
+    const MyInput = ({ ...props }) => {
+        const [field, meta] = useField(props);
+        return (
+            <div className="form__fieldWrapper">
+                <input className='form__input' {...field} {...props} />
+                {meta.touched && meta.error ? <div className="form__err">{meta.error}</div> : <div className="form__err"></div>}
+            </div>
+        );
+    }
+    const MyMessage = ({ ...props }) => {
+        const [field, meta] = useField(props);
+        return (
+            <div className="form__fieldWrapper">
+                <textarea className="form__message"{...field} {...props} />
+                {meta.touched && meta.error ? <div className="form__err">{meta.error}</div> : <div className="form__err"></div>}
+            </div>
+        );
+    }
+    return (
+        <Formik
+            initialValues={{
+                topic: '',
+                email: '',
+                message: '',
+            }}
+            validationSchema={Yup.object({
+                topic: Yup.string()
+                    .min(5, 'Must be at least 5 characters.')
+                    .max(15, "It's too long!")
+                    .required('Required.'),
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required.'),
+                message: Yup.string()
+                    .min(10, 'Your message is too short :(')
+                    .required('Required.'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+                setTimeout(() => {
+                    alert("Thank you for your message!");
+                    resetForm();
+                    setSubmitting(false);
+                }, 2000)
+            }}
+        >{props => (
+            <div className="contact">
+                <form className="form" onSubmit={props.handleSubmit}>
+                    <h1 className="form__header">Write to me!</h1>
+                    <MyInput placeholder="Topic" name="topic" label="Topic" />
+                    <MyInput type="email" name="email" placeholder="E-mail" label="e-mail" />
+                    <MyMessage placeholder="Message..." name="message" />
+                    <button className="form__submit" type="submit">{props.isSubmitting ? "Loading..." : "Submit!"}</button>
+                </form>
+                <div className="follow">
+                    <h1 className="follow__header">Follow me!</h1>
+                    <Link to="https://www.instagram.com/nurthixblog/" className="follow__instagram">
+                        <FontAwesomeIcon icon={faInstagram} size="1x" /> nurthix</Link>
+                </div>
+            </div>
+        )}
+        </Formik>
+    );
+};
+export default ContactPage
