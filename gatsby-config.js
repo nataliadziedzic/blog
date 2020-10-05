b63291e54e67b2d3d41dcea1882a0e573bbefa57
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-10-05 14:54:49 +0200

    Install fontawesome

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-layout`,
     {
       resolve: `gatsby-source-filesystem`,
