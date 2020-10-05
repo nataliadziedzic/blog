1e055cb40f308bd2a72dbf96c10ea64db0b24fb0
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-10-05 10:16:35 +0200

    Create About page

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const AboutPage = ({ data }) => {
+  return (
+    <div className="about">
+      <Img fixed={data.datoCmsAbout.myPicture.fixed} />
+      <div className="about__bio">
+        <h1 className="about__title">Let's get to know each other!</h1>
+        <p className="about__description">{data.datoCmsAbout.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export const query = graphql`
+  {
+    datoCmsAbout {
+      description
+      myPicture {
+        fixed(width: 450) {
+          ...GatsbyDatoCmsFixed_noBase64
+        }
+      }
+    }
+  }
+`
+export default AboutPage
